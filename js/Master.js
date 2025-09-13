3735ea153daf625c1315b2286bfc1d088b81ed6c
Mohamedeid18 <grrgrggrf@outlook.com>
2025-09-14 00:38:19 +0300

    second commit

--- js/Master.js
+++ js/Master.js
@@ -1,3 +1,33 @@
+// Toggle Settings Box
+if(localStorage.getItem('color-option') !== null) {
+  document.documentElement.style.setProperty('--main-color', localStorage.getItem('color-option'));
+  document.querySelectorAll('.colors-list li').forEach(element => {
+    element.classList.remove('active');
+    if(element.getAttribute('data-color') === localStorage.getItem('color-option')) {
+    element.classList.add('active');
+  }
+  });
+
+}
+document.querySelector('.setting-box .toggle-setting i').addEventListener('click', function() {
+  this.classList.toggle('fa-spin');
+  document.querySelector('.setting-box').classList.toggle('switch-setting-box');
+});
+const colorOptions = document.querySelectorAll('.colors-list .color-option');
+colorOptions.forEach(li => {
+  li.addEventListener('click', (e)=> {
+    const selectedColor = e.target.getAttribute('data-color');
+    document.documentElement.style.setProperty('--main-color', selectedColor);
+    localStorage.setItem('color-option', selectedColor);
+    e.target.parentElement.querySelectorAll('.active').forEach(element => {
+      element.classList.remove('active');
+    });
+
+    e.target.classList.add('active');
+  });
+});
+
+// Change Landing Page Background Image
 const landingPage = document.querySelector('.landing-page');
 const landingPageBackgrounds = [
   'url("image/0.jpg")',
