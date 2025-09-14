82c4823951f960de229e2c1f7f49461ea328e1c3
Mohamedeid18 <grrgrggrf@outlook.com>
2025-09-14 03:31:04 +0300

    end first section

--- js/Master.js
+++ js/Master.js
@@ -7,12 +7,22 @@ if(localStorage.getItem('color-option') !== null) {
     element.classList.add('active');
   }
   });
-
 }
+if(localStorage.getItem('background-option') !== null) {
+  document.querySelectorAll('.setting-box .option-content span').forEach(element => {
+    element.classList.remove('active');
+    if(element.getAttribute('data-background') === localStorage.getItem('background-option')) {
+      element.classList.add('active');
+    }
+  });
+}
+
+// Toggle Settings Box
 document.querySelector('.setting-box .toggle-setting i').addEventListener('click', function() {
   this.classList.toggle('fa-spin');
   document.querySelector('.setting-box').classList.toggle('switch-setting-box');
 });
+// Change Theme Color
 const colorOptions = document.querySelectorAll('.colors-list .color-option');
 colorOptions.forEach(li => {
   li.addEventListener('click', (e)=> {
@@ -26,6 +36,28 @@ colorOptions.forEach(li => {
     e.target.classList.add('active');
   });
 });
+// Random Background Option
+
+let backgroundInterval;
+const randomBackgrounds = document.querySelectorAll('.setting-box .option-content span');
+randomBackgrounds.forEach(span => {
+  span.addEventListener('click', (e) => {
+    const backgroundOption = e.target.getAttribute('data-background');
+    localStorage.setItem('background-option', backgroundOption);
+    e.target.parentElement.querySelectorAll('.active').forEach(element => {
+      element.classList.remove('active');
+    });
+
+    e.target.classList.add('active');
+    // Handle Background Option
+    if(e.target.getAttribute('data-background')) {
+      randomBackgroundsFunc(localStorage.getItem('background-option'));
+    } else {
+      randomBackgroundsFunc(localStorage.getItem('background-option'));
+    }
+  });
+});
+
 
 // Change Landing Page Background Image
 const landingPage = document.querySelector('.landing-page');
@@ -36,7 +68,16 @@ const landingPageBackgrounds = [
   'url("image/3.jpg")',
   'url("image/4.jpg")',
 ];
-setInterval(() => {
+
+function randomBackgroundsFunc(x) {
+  if( x ==='Yes') {
+  backgroundInterval = setInterval(() => {
   const randomIndex = Math.floor(Math.random() * landingPageBackgrounds.length);
   landingPage.style.backgroundImage = landingPageBackgrounds[randomIndex];
 }, 5000);
+  }else {
+    clearInterval(backgroundInterval);
+  }
+}
+
+randomBackgroundsFunc(localStorage.getItem('background-option'));
